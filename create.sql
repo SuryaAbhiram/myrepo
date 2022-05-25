c951c133d4914c56336e9b4e5ba3ffe5818285ea
Surya Abhiram <51814191+SuryaAbhiram@users.noreply.github.com>
2022-05-25 16:46:17 +0530

    Update create.sql

--- create.sql
+++ create.sql
@@ -1,1 +1,1 @@
-CREATE TABLE Persons (PersonID int,LastName varchar(255),FirstName varchar(255), Address varchar(255), City varchar(255));
+CREATE TABLE People (PersonID int,LastName varchar(255),FirstName varchar(255), Address varchar(255), City varchar(255));
