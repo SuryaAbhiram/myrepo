e402e4e733a3898c32ca842cc8111d904c23982a
Surya Abhiram <51814191+SuryaAbhiram@users.noreply.github.com>
2022-05-25 16:30:13 +0530

    Add files via upload

---
+++ insert.sql
@@ -0,0 +1,17 @@
+INSERT INTO Persons (
+    PersonID,
+    LastName,
+    FirstName,
+    Address,
+    City
+)VALUES (1,"Abhi","Ram","123","Hyd");
+
+INSERT INTO Persons (
+    PersonID,
+    LastName,
+    FirstName,
+    Address,
+    City
+)VALUES (2,"Rohit","Surampally","456","Hyd");
+
+Select * from Persons
