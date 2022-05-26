b50bebdef31b5e92ebbc2052d26c5b32362f1506
Surya Abhiram <51814191+SuryaAbhiram@users.noreply.github.com>
2022-05-26 10:16:35 +0530

    Update test.groovy

--- test.groovy
+++ test.groovy
@@ -1,19 +1,20 @@
 pipeline {
     agent any
-
-    stages {
-        stage('DDL') {
-            steps {
-                sh(script:"""
-				mysql -h suryadb22.cvirjo0fysae.us-east-1.rds.amazonaws.com -P 3306 -u suryadb22 -psuryadb22 -D test < create.sql 
-                """)
-            }
-        }stage('DML'){
-            steps{
-                sh(script:"""
-                mysql -h suryadb22.cvirjo0fysae.us-east-1.rds.amazonaws.com -P 3306 -u suryadb22 -psuryadb22 -D test < insert.sql
-                """)
-            }
-        }
+        stages {
+             stage('Deploy1'){
+                 steps{
+                       sh '''#!/bin/bash
+                       mysql -h suryadb22.cvirjo0fysae.us-east-1.rds.amazonaws.com -P 3306 -u suryadb22 -psuryadb22 -D test < create.sql 
+                      '''
+                      }
+                    }
+                 stage('Deploy2'){
+                    steps{
+                        sh '''#!/bin/bash
+                        mysql -h suryadb22.cvirjo0fysae.us-east-1.rds.amazonaws.com -P 3306 -u suryadb22 -psuryadb22 -D test < insert.sql 
+                      '''
+                      }
+                      
+                    } 
+            }    
     }
-}
