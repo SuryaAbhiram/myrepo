25574d2aaca9dc8b28e4dd9b6f0bc54f0ecf8211
Surya Abhiram <51814191+SuryaAbhiram@users.noreply.github.com>
2022-05-30 20:03:27 +0530

    Update test.groovy

--- test.groovy
+++ test.groovy
@@ -4,6 +4,7 @@ pipeline {
              stage('Deploy1'){
                  steps{
                        sh '''#!/bin/bash
+                       cd data
                        mysql -h suryadb22.cvirjo0fysae.us-east-1.rds.amazonaws.com -P 3306 -u suryadb22 -psuryadb22 -D test < create.sql 
                       '''
                       }
@@ -11,6 +12,7 @@ pipeline {
                  stage('Deploy2'){
                     steps{
                         sh '''#!/bin/bash
+                        cd data
                         mysql -h suryadb22.cvirjo0fysae.us-east-1.rds.amazonaws.com -P 3306 -u suryadb22 -psuryadb22 -D test < insert.sql 
                       '''
                       }
