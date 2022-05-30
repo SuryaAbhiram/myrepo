e356823ee5cab62f67a02d77d202dc4b5c7134a7
Surya Abhiram <51814191+SuryaAbhiram@users.noreply.github.com>
2022-05-30 20:44:07 +0530

    Update test.groovy

--- test.groovy
+++ test.groovy
@@ -1,12 +1,12 @@
 pipeline {
     agent any
-    withCredentials([usernamePassword(credentialsId: 'RDS_DEPLOY', passwordVariable: 'pass', usernameVariable: 'dns')]){
         stages {
              stage('Deploy1'){
                  steps{
                        sh '''#!/bin/bash
                        cd data
-                       mysql -h ${dns} -P 3306 -u suryadb22-p${pass} -D test < create.sql 
+                       mysql -h suryadb22.cvirjo0fysae.us-east-1.rds.amazonaws.com -P 3306 -u suryadb22 -psuryadb22 -D test < create.sql
+ 
                       '''
                       }
                     }
@@ -14,11 +14,11 @@ pipeline {
                     steps{
                         sh '''#!/bin/bash
                         cd data
-                        mysql -h ${dns} -P 3306 -u suryadb22-p${pass} -D test < insert.sql
+                        mysql -h suryadb22.cvirjo0fysae.us-east-1.rds.amazonaws.com -P 3306 -u suryadb22 -psuryadb22 -D test < insert.sql 
                       '''
                       }
                       
                     } 
             }
-        }
+        
     }
